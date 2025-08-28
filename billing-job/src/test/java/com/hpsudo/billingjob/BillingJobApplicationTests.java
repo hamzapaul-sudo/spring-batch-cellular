ea09c26576c8fe0b90e8ab0c3d8b826847aeeaba
hamzapaul-sudo <hamzapaul987@gmail.com>
2025-08-28 23:58:40 +0200

    optimized tests using @SpringBatchTest

--- billing-job/src/test/java/com/hpsudo/billingjob/BillingJobApplicationTests.java
+++ billing-job/src/test/java/com/hpsudo/billingjob/BillingJobApplicationTests.java
@@ -1,9 +1,13 @@
 package com.hpsudo.billingjob;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
@@ -12,27 +16,33 @@ import org.springframework.boot.test.system.OutputCaptureExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBatchTest
 @SpringBootTest
 @ExtendWith(OutputCaptureExtension.class)
 class BillingJobApplicationTests {
 
     @Autowired
-    private Job job;
+    private JobLauncherTestUtils jobLauncherTestUtils;
 
     @Autowired
-    private JobLauncher jobLauncher;
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @BeforeEach
+    public void setUp() {
+        this.jobRepositoryTestUtils.removeJobExecutions();
+    }
 
     @Test
     void testJobExecution(CapturedOutput output) throws Exception {
 
         //given
-        JobParameters jobParameters = new JobParametersBuilder()
+        JobParameters jobParameters = jobLauncherTestUtils.getUniqueJobParametersBuilder()
                 .addString("input.file", "/some/input/file")
                 .addString("file.format", "csv", false)
                 .toJobParameters();
 
         // when
-        JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
         // assert
         assertTrue(output.getOut().contains("Processing Information from input file /some/input/file"));
